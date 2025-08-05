5633e80669b08bebef8bd0c7e958dee6ec78799c
Ahmad Nuril Huda <ahmadnurilhuda18@gmail.com>
2025-08-05 19:23:38 +0700

    create component footer and homepage

--- next.config.ts
+++ next.config.ts
@@ -10,12 +10,10 @@ const nextConfig: NextConfig = {
         port: '9988',
         pathname: '/uploads/**',
       },
-      // {
-      //   protocol: 'https',
-      //   hostname: '1e12217678df.ngrok-free.app',
-      //   port: '',
-      //   pathname: '/**',
-      // },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
   },
   serverActions: {
